c351467af60100ab746bff8671f18dcac5e1317a
BertLaptop <Bert@laptop>
2015-10-19 10:20:03 +0200

    refactor

--- cb-client/src/main/webapp/node.js
+++ cb-client/src/main/webapp/node.js
@@ -10,19 +10,39 @@ function fetchBackend(fetch){
 }
 
 function start(){
-	require(['node_modules/angular/angular.min.js','node_modules/restangular/dist/restangular.min.js'],function (angular,Restangular){
-		angular.module('your-app', ['restangular']);
+//	require(['node_modules/angular/angular.min.js','node_modules/restangular/dist/restangular.min.js'],function (angular,Restangular){
+//		angular.module('your-app', ['restangular']);
+//
+//		var baseAccounts = Restangular.all('accounts');
+//
+//		// This will query /accounts and return a promise.
+//		baseAccounts.getList().then(function(accounts) {
+//		  $scope.allAccounts = accounts;
+//		});
+//		
+//	});
+	
+	//ff();
+	
+	require(['node_modules/restful.js/dist/restful.standalone.js'],function(restful){
+		var resource = restful('http://localhost:9080/rest/info', fetchBackend(fetch));
 
-		var baseAccounts = Restangular.all('accounts');
-
-		// This will query /accounts and return a promise.
-		baseAccounts.getList().then(function(accounts) {
-		  $scope.allAccounts = accounts;
+		resource.all('info').getAll().then(function(categories) {
+		    var articlePromises = categories.map(function(category) {
+		        if (category.status() !== 200) {
+		            throw new Error('Invalid response');
+		        }
+		        category.data(); // will be the data returned by our API
+		        // category is an entity
+		        // that means you can chain it with other calls
+		        return category.one('articles', 2).get();
+		    });
 		});
 		
 	});
 	
-	//ff();
+	
+	
 	
 }
 
