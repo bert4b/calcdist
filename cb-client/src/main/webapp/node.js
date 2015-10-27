ea48e79ecb9b7c8f721d432f552f918f5990fda1
BertLaptop <Bert@laptop>
2015-10-27 22:07:11 +0100

    part 2

--- cb-client/src/main/webapp/node.js
+++ cb-client/src/main/webapp/node.js
@@ -10,19 +10,7 @@ function fetchBackend(fetch){
 }
 
 function start(){
-//	require(['node_modules/angular/angular.min.js','node_modules/restangular/dist/restangular.min.js'],function (angular,Restangular){
-//		angular.module('your-app', ['restangular']);
-//
-//		var baseAccounts = Restangular.all('accounts');
-//
-//		// This will query /accounts and return a promise.
-//		baseAccounts.getList().then(function(accounts) {
-//		  $scope.allAccounts = accounts;
-//		});
-//		
-//	});
-	
-	//ff();
+
 	
 	require(['node_modules/restful.js/dist/restful.standalone.js'],function(restful){
 		var resource = restful('http://localhost:9080/cb-server/rest/connect', fetchBackend(fetch));
